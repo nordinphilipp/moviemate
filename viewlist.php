b04a1fd663f196baabfd2009995cdfcd7efa385a
Philip Nordin <50575277+nordinphilipp@users.noreply.github.com>
2022-01-10 22:59:32 +0100

    Update viewlist.php

--- viewlist.php
+++ viewlist.php
@@ -1,8 +1,8 @@
 <?php
     include('include/bootstrap.php');
-	include('include/methods/functions.php');
-	include('include/process/fetchUser_process.php');
+    include('include/methods/functions.php');
+    include('include/process/fetchUser_process.php');
     include('include/views/viewlist.php');
-	include('include/process/connect_process.php');
+    include('include/process/connect_process.php');
 	
 ?>
