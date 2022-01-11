b3fc1a405e5beb1f4b87fc465387836f2695b5d1
Philip Nordin <50575277+nordinphilipp@users.noreply.github.com>
2022-01-11 08:22:53 +0100

    Update rating.php

--- rating.php
+++ rating.php
@@ -1,34 +1,31 @@
 <?php
+<?php
 include('include/bootstrap.php');
 include('include/process/fetchUser_process.php');
 include('include/process/connect_process.php');
 include('include/methods/functions.php');
+//Använd include/methods/rating.php- filen istället för denna<-----
 
-$user = "root";
-$pw = "";
-$host = "localhost";
-$db = "moviemate";
-$connect = new mysqli($host, $user, $pw, $db);
 
 $rating = $_GET['rating'];
 $userid = $_SESSION['userID'];
 $movieid = $_GET['movie'];
 
 
-$check = $connect->prepare("select * from movies2 where userID = '$userid' and movieID = '$movieid'");
+$check = $connection->prepare("select * from movies2 where userID = '$userid' and movieID = '$movieid'");
 $check->execute();
 $check->store_result();
 $rows = $check->num_rows;
 if($rows > 0)
 {
 	$sql = "update movies2 set rating = '$rating' where userID = '$userid' and movieID = '$movieid'";
-	mysqli_query($connect,$sql);
+	mysqli_query($connection,$sql);
 }
 else
 {
-	$state  = $connect->prepare("INSERT INTO movies2(userID,movieID,rating) VALUES(?,?,?)");
+	$state  = $connection->prepare("INSERT INTO movies2(userID,movieID,rating) VALUES(?,?,?)");
 	$state->bind_param('sss',$userid,$movieid,$rating);
 	$state->execute();
 }
-$connect->close();
+$connection->close();
 ?>
