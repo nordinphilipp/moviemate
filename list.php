6e199a0ffa449658c6956417ef5167b822e12772
Philip Nordin <50575277+nordinphilipp@users.noreply.github.com>
2022-01-11 08:04:22 +0100

    Update list.php

--- list.php
+++ list.php
@@ -2,7 +2,7 @@
     include('include/bootstrap.php');
 	include('include/methods/functions.php');
 	include('include/process/fetchUser_process.php');
-    include('include/views/list.php');
+    	include('include/views/list.php');
 	include('include/process/connect_process.php');
 	
 ?>
