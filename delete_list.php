bfcd6795f02552653f599057a7a4f3d6853842df
Philip Nordin <50575277+nordinphilipp@users.noreply.github.com>
2022-01-11 08:31:13 +0100

    Update delete_list.php

--- delete_list.php
+++ delete_list.php
@@ -7,28 +7,28 @@
 	$res = $listresult -> fetch_array();
 	$userID = $res['userID'];
 	
-		if(!empty($_SESSION['logged_in']))
-		{
-			if($_SESSION['accessLevel'] == 2)
-				{
-					$sql = "DELETE FROM lists WHERE listID='$listID'";
-					mysqli_query($connection, $sql);
-					header("Location: profile.php");
-				}
-			else if($_SESSION['userID'] == $userID)
-				{
-					$sql = "DELETE FROM lists WHERE listID='$listID'";
-					mysqli_query($connection, $sql);
-					header("Location: profile.php");
-				}
-			else
+	if(!empty($_SESSION['logged_in']))
+	{
+		if($_SESSION['accessLevel'] == 2)
+			{
+				$sql = "DELETE FROM lists WHERE listID='$listID'";
+				mysqli_query($connection, $sql);
+				header("Location: profile.php");
+			}
+		else if($_SESSION['userID'] == $userID)
+			{
+				$sql = "DELETE FROM lists WHERE listID='$listID'";
+				mysqli_query($connection, $sql);
+				header("Location: profile.php");
+			}
+		else
 			{
 				echo '<script> displayAlert("Du har ej behörighet")</script>';			
 			}
 		}
 		else
 		{
-			echo '<script> displayAlert("Du har ej behörighet")</script>';
-			
+			echo '<script> displayAlert("Du har ej behörighet")</script>';	
 		}
 ?> 
+
